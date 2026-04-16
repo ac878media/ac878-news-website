356ddaeb8b2692261c41334cc7b28831f28ee2b0
Jacky <jacky.cheung@me.com>
2026-04-16 13:40:49 +1000

    Fix logo background - add white background padding to logo image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
             alt="AC878"
             width={48}
             height={48}
-            className="rounded-lg"
+            className="rounded-lg bg-white p-1"
           />
           <div>
             <span className="text-white text-xl font-bold tracking-tight">878时讯</span>
